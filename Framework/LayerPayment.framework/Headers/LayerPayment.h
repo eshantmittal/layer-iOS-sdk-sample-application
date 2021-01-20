f04e28e78d1206dadba62018bb1fc1ad9b758da3
Karnika Singh <karnika.singh@bankopen.co>
2021-01-20 12:20:03 +0530

    .DS_Store banished!

---
+++ Framework/LayerPayment.framework/Headers/LayerPayment.h
@@ -0,0 +1,20 @@
+//
+//  LayerPayment.h
+//  LayerPayment
+//
+//  Created by Mahesh on 10/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LayerPayment.
+FOUNDATION_EXPORT double LayerPaymentVersionNumber;
+
+//! Project version string for LayerPayment.
+FOUNDATION_EXPORT const unsigned char LayerPaymentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LayerPayment/PublicHeader.h>
+
+
+#import <LayerPayment/LayerPayment.h>
+
